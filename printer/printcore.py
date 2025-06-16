cd2e885414c0326baa41587b7be7237852285616
Ian A. Fights <ian.fights@thefightsgroup.com>
2025-06-16 00:09:43 -0400

    i forgot to make github commits and now this projects early history SUCKS sorry

--- printer/printcore.py
+++ printer/printcore.py
@@ -4,16 +4,14 @@ from PIL import Image
 from PIL import ImageDraw
 from PIL import ImageFont
 from loguru import logger
-from config import format_schema
-from loaders import open_image_cached #should i use these through a namespace? seems like it might get encumberingly verbose
-from loaders import open_font_cached
-from loaders import get_image
+from printer.config import format_schema
+from printer.loaders import open_image_cached, open_font_cached, get_image
 
 Font = ImageFont.ImageFont | ImageFont.FreeTypeFont
 
 # TODO: X position handling through this whole thing is jank af
 
-# fonts TODO: data driven
+# TODO: data driven
 nameFont = ImageFont.truetype('assets/fonts/Poly-Regular.ttf', 63)
 statFont = ImageFont.truetype('assets/fonts/Cambria.otf', 109)
 textFont = ImageFont.truetype('assets/fonts/Cambria.otf', 33)
@@ -30,14 +28,14 @@ def draw_layer(image: Image.Image, items: Sequence[dict], params: dict) -> None:
     pass
 
 #MARK: Assemble Card
-def assemble_card(power: Image.Image | str | int = '0', health: Image.Image | str | int = '0', tier: str = 'tierless', temple: str = 'templeless',
+def assemble_card(power: str | int = '0', health: str | int = '0', rarity: str = 'rarityless', temple: str = 'templeless',
                   tribes: Sequence | None = None, name: str = 'Unnamed Card', artist: str = 'No Artist',
-                  format: str = 'no format string lol', decals : Sequence[tuple[Image.Image|str, tuple[int, int]]] = [], 
+                  format: str = 'no format string lol', decals : Sequence[tuple[Image.Image|str, tuple[int, int]]] = [],
                   accentcolor : tuple[int,int,int,int] | None = None,
                   **kwargs) -> Image.Image:
     """Create and return a card image given the pieces of a card.
 
-    Given the information of a card, return the card as an image. 
+    Given the information of a card, return the card as an image.
     Tribes should be provided as a sequence of strings, even if there
     is only 1 tribe.
 
@@ -47,7 +45,7 @@ def assemble_card(power: Image.Image | str | int = '0', health: Image.Image | st
         Image assets for the card. Accept an image object or a path.
     power, health : string
         Values to print in the card's stat boxes
-    tier, temple : string
+    rarity, temple : string
         Values to print in the card's tribeline
     body: Sequence of BodyRenderer objects
         Sequence of things that go in the card's main body; in the
@@ -89,14 +87,12 @@ def assemble_card(power: Image.Image | str | int = '0', health: Image.Image | st
     draw = ImageDraw.Draw(image)
 
     # Draw stats TODO: Variable power
-    power = typing.cast(int, power)
-    health = typing.cast(int, health)
+    power, health = str(power), str(health)
     for v, x in [(power, 180), (health, 940)]: # TODO: pretend this is forwards compatibility and not laziness
         try:
             length = statFont.getlength(str(int(v)))
             shadow_text(draw,int(x - length/2),1331,str(int(v)),statFont)
         except ValueError: # it was actually a variable power and not an integer
-            v = typing.cast(str | Image.Image, v)
             with get_image(v) as icon:
                 image.alpha_composite(icon, (int(x-icon.width/2), 1351))
     #shadow_text(draw,968,1331,str(int(health)),statFont,anchor="ra")
@@ -105,8 +101,8 @@ def assemble_card(power: Image.Image | str | int = '0', health: Image.Image | st
     for renderstep in format_schema['rendersteps']['hires']:
         renderstep.draw(image, kwargs)
 
-    # Tribeline (includes tier and temple)
-    tribe_string = '{} {}'.format(tier, temple) if tribes == None else '{} {} - {}'.format(tier, temple, ' '.join(tribes))
+    # Tribeline (includes rarity and temple)
+    tribe_string = '{} {}'.format(rarity, temple) if tribes == None else '{} {} - {}'.format(rarity, temple, ' '.join(tribes))
     draw.text((151,1295), tribe_string, fill=accentcolor, font=textFont, anchor="la")
 
     # Draw name
---
+++ printer/text.py
@@ -0,0 +1,134 @@
+import copy
+from typing import Sequence
+from loguru import logger
+from PIL import Image
+from PIL import ImageDraw
+from printer.loaders import open_font_cached 
+from collections import namedtuple
+
+# Named tuple representing one layer of a text style
+StyleLayer = namedtuple('StyleLayer', ('font', 'offset', 'flags', 'color'))
+
+class TextStyle:
+    """Object that represents a way to render text; made of font layers
+    """
+    def __init__(self, layers: Sequence, name: str | None = None):
+        self.name = name
+        self.layers = []
+        # We need to pack the parameters from the config file into this object
+        for layer in layers:
+            style_layer = StyleLayer(
+                # Open the font to use for this text layer
+                open_font_cached(layer['font'], layer['size']),
+                # If we were given one, save the layer offset; otherwise there
+                # is no offset
+                tuple(layer.get('offset', (0,0))),
+                # If we were given any, save the layer flags
+                set(layer.get('flags', set())),
+                # If we were given one, set the color override for this layer.
+                # We need the ternary because we only want a tuple if the color
+                # is found.
+                tuple(layer['color']) if 'color' in layer else None
+            )
+            # Add the style layer to the style
+            self.layers.append(style_layer)
+    
+    def print_line(self, text: str, img: ImageDraw.ImageDraw, 
+                   pos: tuple[int, int], fill: tuple[int, int, int] = (0,0,0), 
+                   **kwargs):
+        """Draw styled text; passes extra args to ImageDraw.text()
+        
+        Parameters
+        ----------
+        img : ImageDraw
+            The image draw object to print onto
+        text : str
+            The text to print
+        pos : tuple[int, int]
+            The position to draw the text
+        color : tuple[int, int, int]
+            The color to draw text in, given that no color is specified by
+            the style.
+        """
+        # Unpack the position tuple for convenience
+        posx, posy = pos
+        # For each style layer...
+        for layer in self.layers:
+            ox, oy = layer.offset
+            # If it was specified use the layer color instead of the line color
+            color = fill if layer.color is None else layer['color']
+            # Draw it at the current position, plus its offset, with the right
+            # color and font
+            img.text((posx+ox, posy+oy), text, font=layer.font,
+                      fill=color, **kwargs)
+    
+    def get_length(self, text: str) -> int:
+        """Get length of a string in this style"""
+        # We start with both borders at the start of the text
+        left_border, right_border = 0, 0
+        # For each style layer...
+        for layer in self.layers:
+            # Ignore it if it's ignored for this purpose (so that things like
+            # dropshadows don't have to mess with text width)
+            if 'ignore for width' in layer.flags:
+                continue
+            # Retrieve it's offsets
+            offset_x, oy = layer.offset
+            # Get the width of the text in this style layer's font
+            width = layer.font.getlength(text)
+            # If its offset to the left further than any previous layer, the 
+            # offset becomes the new left border
+            left_border = min(left_border, offset_x)
+            # If its right border is farther right than the rightmost layer so
+            # far, it becomes the new right border
+            right_border = max(offset_x+width, right_border)
+        # the width of a thing is the distance between the two sides of it
+        return right_border-left_border
+    
+    #TODO: Replace with rich text handling
+    def wrap(self, text: str, w: int, first_line_offset: int = 0) -> list[str]:
+        """Split a string into a list of strings that each fit within a given horizontal space.
+        
+        Parameters
+        ----------
+        text : str
+            The string to be split
+        w : int
+            The width of available space in pixels
+        first_line_offset : int
+            An offset to apply to the first line; negative values can be
+            used as hanging indentation
+        """
+        
+        # The first line can be indented (this is mostly to give room for 
+        # rendering a sigil name seperately)
+        x = first_line_offset
+        lines = []
+        current_line = []
+        # Split the line by spaces, then add a space to each word, to account
+        # for the one we ate in the split
+        for word in (n+' ' for n in text.split()): 
+            # Find the length of the word...
+            length = self.get_length(word)
+            # And if it's bigger than the remaining space on this line...
+            if x + length > w:
+                # We're done with this line; append it to the finished list
+                lines.append(''.join(current_line))
+                # Reset the current line
+                current_line = []
+                # And reset the space left on the current line
+                x = 0
+            # Whether it is or not, now we add that word's width to our current
+            # line
+            x += length
+            # And add the word itself to the curret line.
+            current_line.append(word)
+        # We know there will always be at least one word on a line that isn't
+        # wrapped, because for a wrap to happen, such a word must exist
+        lines.append(''.join(current_line)) 
+        return lines
+    
+class TextBox():
+    """Rectangle that contains text """
+    def __init__(self):
+        return self
