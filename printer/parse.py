0792bcaa381e18b65e4e4e75f1df759801c9a441
Ian A. Fights <ian.fights@thefightsgroup.com>
2026-01-03 13:47:12 -0500

    Implemented CardContext and the ability to load data into it

--- printer/parse.py
+++ printer/parse.py
@@ -1,3 +1,4 @@
+import collections.abc
 from PIL import Image
 from printer import bodyitems
 from printer.loaders import open_image_cached
@@ -9,12 +10,12 @@ class Cost:
     
     Parameters
     ----------
-    icon : Image or path to image
+    icon : Path to image
         The cost icon
     threshold : int, optional
         The threshold at which this cost changes to compact rendering
     """
-    def __init__(self, icon : str, fold : int | None):
+    def __init__(self, icon : str, fold : int | None = None):
         self.icon = open_image_cached(icon)
         self.fold = fold
     
@@ -23,8 +24,57 @@ class Cost:
         return self.fold is None or count <= self.fold
 
 def parse_sigil(raw : dict) -> bodyitems.Sigil:
+    """Parse the dictionary form of a sigil into the Sigil class"""
     # All the work is done by the sigil initialization function
     return bodyitems.Sigil(**raw)
 
 def parse_cost(raw : dict) -> Cost:
+    """Parse the dictionary form of a cost into the Cost class"""
     return Cost(raw["icon"], raw["fold"])
+
+# NOTE: This might be a good place to split this file later;
+# I'm not quite sure if parsing the items and handling complete files should 
+# be multiple files
+
+class CardContext:
+    """Contextual information for the printing of cards"""
+    def __init__(self):
+        self.sigils = {}
+        self.costs = {}
+    
+    def load(self, raw : dict):
+        """Parse a set of sigils or costs and add them to this context"""
+        # Determine what kind of entry we're loading
+        # TODO: Retroactively don't repeat yourself
+        match raw["type"]: 
+            # If we're loading costs
+            case "costs":
+                # For each cost
+                for raw_cost in raw["contents"]:
+                    # Parse the cost's actual data
+                    cost = parse_cost(raw_cost)
+                    # Allow cost names to be specified alone in the format by
+                    # making a single cost into a tuple of length one
+                    if isinstance(raw_cost["name"], str):
+                        names = (raw_cost["name"],)
+                    else:
+                        names = raw_cost["name"]
+                    # Then for each name 
+                    for name in names:
+                        # Assign said cost to the name 
+                        self.costs[name] = cost
+            case "sigils":
+                # For each sigil
+                for raw_sigil in raw["contents"]:
+                    # Parse the sigil's actual data
+                    sigil = parse_cost(raw_sigil)
+                    # Allow sigil names to be specified alone in the format by
+                    # making a single cost into a tuple of length one
+                    if isinstance(raw_sigil["name"], str):
+                        names = (raw_sigil["name"],)
+                    else:
+                        names = raw_sigil["name"]
+                    # Then for each name 
+                    for name in names:
+                        # Assign said cost to the name 
+                        self.sigils[name] = sigil
