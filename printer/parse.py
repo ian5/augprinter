568d0048262b5ded39e3f7fc8329b5cbaed03147
Ian A. Fights <ian.fights@thefightsgroup.com>
2026-01-03 02:39:56 -0500

    Defined Cost

--- printer/parse.py
+++ printer/parse.py
@@ -1,5 +1,26 @@
-import yaml
+from PIL import Image
 from printer import bodyitems
 
+# I can't think of a better place for this right now; it's an object that
+# should exist but doesn't fit into the category of any of the others
+class Cost:
+    """Definition of a particular cost
+    
+    Parameters
+    ----------
+    icon : Image or path to image
+        The cost icon
+    threshold : int, optional
+        The threshold at which this cost changes to compact rendering               
+    """
+    def __init__(self, icon : str | Image.Image, threshold : int | None):
+        self.icon = icon
+        self.threshold = threshold
+    
+    def get_compact(self, count : int) -> bool:
+        """Returns true if the given count of this cost should be compact"""
+        return self.threshold is None or count <= self.threshold
+
 def parse_sigil(raw : dict) -> bodyitems.Sigil:
+    # All the work is done by the sigil initialization function
     return bodyitems.Sigil(**raw)
