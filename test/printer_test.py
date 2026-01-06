7b47bfa0e4a2920e9932dc7874e9761eb523b22d
Ian A. Fights <ian.fights@thefightsgroup.com>
2026-01-06 03:44:39 -0500

    Updated printer tests for the new cost model

--- test/printer_test.py
+++ test/printer_test.py
@@ -4,6 +4,7 @@ from helper import assert_image_equal_tofile
 from printer.bodyitems import (
     BodyItem, Sigil, Conditional, InfoBox, HorizontalRule, Trait, FlavorText
 )
+from printer.parse import Cost
 import printer.printcore as printcore
 
 @pytest.mark.parametrize('standard_path,carddefinition', [
@@ -13,14 +14,14 @@ import printer.printcore as printcore
         Sigil('Long Test', 'assets/sigils/Bone Digger.png', "This sigil has very long text that is being used to ensure text wrapping hasn't blown up somehow. Here's a few more words, so it's taller than the icon."),
         Sigil('Test Sigil', 'assets/sigils/Brittle.png', 'This sigil is short.'), 
         InfoBox([
-            Sigil('White Sigil', 'assets/sigils/Airborne.png', "This sigil is being rendered in an infobox.", blacked=True)]
+            Sigil('White Sigil', 'assets/sigils/Airborne.png', "This sigil is being rendered in an infobox.")]
         )],
     'tribes': ['Test Card'], 'rarity': 'Fake As Shit', 'temple': 'Undead', 
     'power': 1, 'health': 1, 'name': 'Sigil Tester', 
     'artist': 'PixelProfligate', 'costs': [], 'format': 'TEST'}),
     ('test/example/bodyitemtester.png', {
         'portrait':'assets/art/Child 13.png', 'background':'assets/bg/bg_rare_beast.png', 'frame':'assets/frames/frame_rare_beast.png', 'artist':'Raytheon',
-                'tribes':['Hooved', 'Cryptid'], 'rarity':'Fake As Shit', 'temple':'Beast', 'power':4, 'health':4, 'name':'BodyItem Tester', 'costs':[('assets/cost/blood.png', 1, False)], 'format':'TEST',
+                'tribes':['Hooved', 'Cryptid'], 'rarity':'Fake As Shit', 'temple':'Beast', 'power':4, 'health':4, 'name':'BodyItem Tester', 'costs':[(Cost('assets/cost/blood.png', 5), 1)], 'format':'TEST',
                 'mainbody' : [  
                     Trait('This card is being used as a pretty inelegant test for the card printer.'),
                     Conditional('assets/misc/VALORCELL.png'),
@@ -29,7 +30,7 @@ import printer.printcore as printcore
                 ]}),
     ('test/example/othertester.png', {
         'portrait':'assets/art/Skel-e-latcher.png', 'background':'assets/bg/bg_common_magick.png', 'frame':'assets/frames/frame_common_magick.png', 'artist':'Raytheon',
-                'tribes':['Latcher'], 'rarity':'Fake As Shit', 'temple':'Tech', 'power':'assets/variable/Mirror Power.png', 'health':1, 'name':'OtherTester', 'costs':[('assets/cost/energy.png', 5, True), ('assets/cost/overcharge.png', 2, True)], 'format':'TEST',
+                'tribes':['Latcher'], 'rarity':'Fake As Shit', 'temple':'Tech', 'power':'assets/variable/Mirror Power.png', 'health':1, 'name':'OtherTester', 'costs':[(Cost('assets/cost/energy.png', 7), 5), (Cost('assets/cost/overcharge.png', 7), 2)], 'format':'TEST',
                 'decals' : [('assets/misc/gems/moxband_3empty.png', (40, 840)), ('assets/misc/gems/gemO.png', (440, 850))], 'mainbody' : [
                     FlavorText("This card is being used to test decals and variable powers, so it doesn't actually need a meaningful body. I was gonna give you a funny big infobox instead, but apparently flavortext doesn't work when rendered inside of an infobox, and given that that's not really real behavior, I've elected not to fix it, so you have to settle with funny long flavortext instead.")
                 ]})
